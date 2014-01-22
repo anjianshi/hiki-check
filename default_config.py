cafb33a5969ed4dd98bddab7e1f8b8b1e219eb1b
anjianshi <anjianshi@gmail.com>
2014-01-22 13:55:54 +0800

    改为只支持 SAE

--- default_config.py
+++ default_config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
 device_name = None
-interval = 60  # 两次检查间间隔多少秒
-connect_timeout = 10 # 通过网络连接目标硬盘录像机允许的时长
+connect_timeout = 10 # 通过网络连接目标硬盘录像机允许的时长，由于 SAE Cron 限制，不能超过 300 秒
+mail_smtp = None # (host, port, mail_account, mail_password, use_tls)
+mail_receiver = None 
+mobile_number = None
--- hiki_check.py
+++ hiki_check.py
@@ -5,9 +5,18 @@ from datetime import datetime
 import requests
 from requests.exceptions import Timeout
 import sys
+from sae.mail import send_mail
+import urllib, urllib2
+
+# 解决 SAE 中 requests 无法正常访问外网的问题
+import os
+os.environ['disable_fetchurl'] = True
 
 from default_config import *
 from config import *
+
+if device_name is None:
+  raise Exception(u'please edit this file, specify a device name')
 
 
 def write_log(location, normal=True):
@@ -16,17 +25,20 @@ def write_log(location, normal=True):
     location=location,
     time=datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
   )
+
   print(content, end='\n', file=sys.stdout if normal else sys.stderr)
 
-  if not normal:
-    # todo： 发邮件、发短信
-    pass
+  if mail_receiver is not None and mail_smtp is not None:
+    send_mail(mail_receiver, "hiki-check {}".format('normal' if normal else 'failed'), content, mail_smtp)
+    
+  # todo: 发 QQ (twqq) 或短信
 
 
-if device_name is None:
-  raise Exception(u'please edit this file, specify a device name')
+def execute(log_when_normal=False):
+  """默认只在 error 的情况下写日志，如果想要无论如何都写一条日志（并发邮件和短信），
+  可以在调用时把 log_when_normal 设为 True。
+  这个功能可以用来确认脚本是否运行正常"""
 
-while True:
   resp = requests.get('http://www.hik-online.com/' + device_name, allow_redirects=False)
   location = resp.headers['location']
 
@@ -34,7 +46,8 @@ while True:
     requests.get(location, timeout=connect_timeout)
   except Timeout:
     write_log(location, False)
+    return 'error'
   else:
-    write_log(location)
-
-  time.sleep(interval)
+    if log_when_normal:
+      write_log(location)
+    return 'normal'
