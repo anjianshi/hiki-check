bf4f489d09883bfc11d4a5eefa118bc5c21d2f84
anjianshi <anjianshi@gmail.com>
2014-01-28 13:45:35 +0800

    fix config

--- default_config.py
+++ default_config.py
@@ -3,8 +3,8 @@
 app_port = 5000
 
 device_name = None
-check_interval = 30 # 两次检查间隔多长时间（单位:秒），默认是5分钟
-connect_timeout = 10 # 通过网络连接目标硬盘录像机允许的时长
+check_interval = 300 # 两次检查间隔多长时间（单位:秒），默认是5分钟
+connect_timeout = 30 # 通过网络连接目标硬盘录像机允许的时长
 
 mail_smtp = None # (host, port, mail_account, mail_password, use_tls)
 mail_receiver = None 
