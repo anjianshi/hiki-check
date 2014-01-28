2b144526b203c8d5933c44cf2201718098e3810c
anjianshi <anjianshi@gmail.com>
2014-01-28 13:20:38 +0800

    重构，不再支持SAE，改为支持标准 python 环境；支持发送短信

--- default_config.py
+++ default_config.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
+check_interval = 30 # 两次检查间隔多长时间（单位:秒），默认是5分钟
 device_name = None
-connect_timeout = 10 # 通过网络连接目标硬盘录像机允许的时长，由于 SAE Cron 限制，不能超过 300 秒
+connect_timeout = 10 # 通过网络连接目标硬盘录像机允许的时长
+
 mail_smtp = None # (host, port, mail_account, mail_password, use_tls)
 mail_receiver = None 
-mobile_number = None
+
+sms_api_key = None
+sms_receiver = None
--- hiki_check.py
+++ hiki_check.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-import time
 from datetime import datetime
 import requests
 from requests.exceptions import Timeout
-import sys
-from sae.mail import send_mail
-import urllib, urllib2
-
-# 解决 SAE 中 requests 无法正常访问外网的问题
+from flask import Flask
 import os
-os.environ['disable_fetchurl'] = True
+import threading
+import time
+import smtplib
+from email.mime.text import MIMEText
+
 
 from default_config import *
 from config import *
@@ -19,25 +17,9 @@ if device_name is None:
   raise Exception(u'please edit this file, specify a device name')
 
 
-def write_log(location, normal=True):
-  content = '{result} , {location} , {time}'.format(
-    result='normal' if normal else 'error',
-    location=location,
-    time=datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-  )
-
-  print(content, end='\n', file=sys.stdout if normal else sys.stderr)
-
-  if mail_receiver is not None and mail_smtp is not None:
-    send_mail(mail_receiver, "hiki-check {}".format('normal' if normal else 'failed'), content, mail_smtp)
-    
-  # todo: 发 QQ (twqq) 或短信
-
-
-def execute(log_when_normal=False):
-  """默认只在 error 的情况下写日志，如果想要无论如何都写一条日志（并发邮件和短信），
-  可以在调用时把 log_when_normal 设为 True。
-  这个功能可以用来确认脚本是否运行正常"""
+def check(skip_logging=False):
+  """执行检查，若发现错误，则写入日志
+  把 skip_logging 设为 True 则即使检查失败也不写日志"""
 
   resp = requests.get('http://www.hik-online.com/' + device_name, allow_redirects=False)
   location = resp.headers['location']
@@ -45,9 +27,82 @@ def execute(log_when_normal=False):
   try:
     requests.get(location, timeout=connect_timeout)
   except Timeout:
-    write_log(location, False)
+    if not skip_logging:
+      _write_failed_log(location)
     return 'error'
   else:
-    if log_when_normal:
-      write_log(location)
     return 'normal'
+
+
+def _write_failed_log(location):
+  content = 'check failed, {location} , {time}'.format(
+    location=location,
+    time=datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+  )
+
+  with open('check.log', 'a') as log_file:
+    log_file.write(content + "\n")
+    _send_mail("hiki-check failed", content)
+    _send_sms()
+
+
+def _send_mail(subject, content):
+  """发邮件，必须在 config 文件里指定 mail_receiver 和 mail_smtp 才能发送"""
+  if mail_receiver is None or mail_smtp is None:
+    return False
+
+  host, port, mail_account, mail_password, use_tls = mail_smtp
+
+  msg = MIMEText(unicode(content).encode('utf-8'))
+  msg['Subject'] = unicode(subject).encode('utf-8')
+  msg['From'] = mail_account
+  msg['To'] = mail_receiver
+  msg = msg.as_string()
+
+  _cls = smtplib.SMTP_SSL if use_tls else smtplib.SMTP
+  s = _cls(host, port)
+  s.login(mail_account, mail_password)
+  s.sendmail(mail_account, mail_receiver, msg)
+
+  return True
+
+
+def _send_sms():
+  """通过推立方平台(www.tui3.com)发短信，必须在 config 文件里指定 sms_api_key 和 sms_receiver 才能发送
+  为避免内容过长被拆分为2条，只给出最简化的通知信息
+  """
+  if sms_api_key is None or sms_receiver is None:
+    return False
+
+  content = '您的注册信息为: hiki failed'
+  resp = requests.post('http://www.tui3.com/api/send/', 
+    data=dict(k=sms_api_key, p=1, t=sms_receiver, c=unicode(content).encode('utf-8')))
+  return True
+
+
+if __name__ == "__main__":
+  # 每隔固定时长进行一次检查
+  # 必须在 app 运行前执行，不然无效
+  def thread_logic():
+    while True:
+      print 'result: ' + check()
+      time.sleep(check_interval)
+  
+  threading.Thread(target=thread_logic).start()
+
+
+  app = Flask(__name__)
+
+  @app.route('/')
+  def web_logic():
+    """访问此网址会强制执行一次检查，并返回 check.log 中的日志内容"""
+    text = 'check result: ' + check(True)
+
+    if os.path.exists('check.log'):
+      with open('check.log', 'r') as log_file:
+        text += "\n\n\nlog content:\n"
+        text += log_file.read()
+
+    return '<pre>' + text + '</pre>'
+
+  app.run()
